8ea2eb3b1524b3759cd9f82dedaea7fee26c9f04
vinyscordeiro <vinyscordeiro@gmail.com>
2020-10-28 22:44:47 +0000

    backend

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express,{Request, Response, NextFunction} from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -10,6 +11,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use((err:Error, request:Request, response:Response, next:NextFunction) => {
